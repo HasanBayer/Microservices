ff355215a56e3cd6e93f745569cec5b6fac242ae
Hasan Bayer <hasanbayer@gmail.com>
2024-01-28 13:58:11 +0300

    Course ve Category Controller Eklendi

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Controller/CategoryController.cs
@@ -0,0 +1,37 @@
+﻿using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Services.Catalog.Services;
+using FreeCourse.Shared.ControllerBases;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Services.Catalog.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : CustomBaseController
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public async Task<IActionResult> GetAll()
+        {
+            var category = await _categoryService.GetAllAsync();
+            return CreateActionResultInstance(category);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var category = await _categoryService.GetByIdAsync(id);
+            return CreateActionResultInstance(category);
+        }
+        public async Task<IActionResult> Create(CategoryDto categoryDto)
+        {
+            var response = await _categoryService.CreateAsync(categoryDto);
+            return CreateActionResultInstance(response);
+        }
+    }
+}
