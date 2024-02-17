ab2b337fcbd5f2a5a0f1c16632e4814ee5d59d46
Hasan Bayer <hasanbayer@gmail.com>
2024-02-17 23:09:15 +0300

    BasketService Eklendi

--- Services/Basket/FreeCourse.Services.Basket/Program.cs
+++ Services/Basket/FreeCourse.Services.Basket/Program.cs
@@ -1,6 +1,23 @@
+using FreeCourse.Services.Basket.Services;
+using FreeCourse.Services.Basket.Settings;
+using FreeCourse.Shared.Services;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.Configure<RedisSettings>(builder.Configuration.GetSection("RedisDBSettings"));
+builder.Services.AddSingleton<RedisService>(sp =>
+{
+    var redisSettings = sp.GetRequiredService<IOptions<RedisSettings>>().Value;
+    var redis=new RedisService(redisSettings.Host,redisSettings.Port);
+    redis.Connect();
+    return redis;
+});
+builder.Services.AddSingleton<BasketService>();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<ISharedIdentityService,SharedIdentityService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ Services/Basket/FreeCourse.Services.Basket/Settings/RedisSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace FreeCourse.Services.Basket.Settings
+{
+    public class RedisSettings
+    {
+        public int Port { get; set; }
+        public string Host { get; set; }
+    }
+}
