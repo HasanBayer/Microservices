f5b9137be85bcf21607003539bb47a1c49311277
Hasan Bayer <hasanbayer@gmail.com>
2024-02-29 15:11:43 +0300

    Order tüm hatalar düzeltildi, Order2 yerine eklendi

---
+++ Services/Order/FreeCourse.Services.Order.Application/Dtos/OrderItemDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Order.Application.Dtos
+{
+    public class OrderItemDto
+    {
+        public string CourseId { get; set; }
+        public string CourseName { get; set; }
+        public string CourseImageUrl { get; set; }
+        public Decimal Price { get; set; }
+    }
+}
---
+++ Services/Order/FreeCourse.Services.Order.Domain/OrderAggregate/OrderItem.cs
@@ -0,0 +1,31 @@
+﻿using FreeCourse.Services.Order.Domain.Core;
+
+namespace FreeCourse.Services.Order.Domain.OrderAggregate
+{
+    public class OrderItem : Entity
+    {
+        public string CourseId { get; private set; }
+        public string CourseName { get; private set; }
+        public string CourseImageUrl { get; private set; }
+        public Decimal Price { get; private set; }
+
+        public OrderItem()
+        {
+        }
+
+        public OrderItem(string productId, string productName, string pictureUrl, decimal price)
+        {
+            CourseId = productId;
+            CourseName = productName;
+            CourseImageUrl = pictureUrl;
+            Price = price;
+        }
+
+        public void UpdateOrderItem(string courseName, string courseImageUrl, decimal price)
+        {
+            CourseName = courseName;
+            Price = price;
+            CourseImageUrl = courseImageUrl;
+        }
+    }
+}
