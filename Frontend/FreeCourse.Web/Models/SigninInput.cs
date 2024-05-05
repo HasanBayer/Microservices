15d6d995c17207bfef223bee93c78d4ad83b2605
Hasan Bayer <hasanbayer@gmail.com>
2024-05-05 17:47:30 +0300

    IIdentityService/IdentityModel Eklendi

---
+++ Frontend/FreeCourse.Web/Models/SigninInput.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FreeCourse.Web.Models
+{
+    public class SigninInput
+    {
+        [Display(Name = "Email Giriniz")]
+        public string Email { get; set; }
+        [Display(Name = "Şifrenizi Giriniz")]
+        public string Password { get; set; }
+        [Display(Name = "Beni Hatırla")]
+        public bool IsRemember { get; set; }
+    }
+}
