5b7a5721bfa34a1d684ea8ddf0ede3cd17f150f4
Hasan Bayer <hasanbayer@gmail.com>
2024-06-13 12:57:48 +0300

    ICatalogService eklendi

---
+++ Frontend/FreeCourse.Web/Models/Catalogs/FeatureViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FreeCourse.Web.Models.Catalogs
+{
+    public class FeatureViewModel
+    {
+        public int Duration { get; set; }
+
+    }
+}
---
+++ Frontend/FreeCourse.Web/Services/Interfaces/ICatalogService.cs
@@ -0,0 +1,16 @@
+﻿using FreeCourse.Web.Models.Catalogs;
+
+namespace FreeCourse.Web.Services.Interfaces
+{
+    public interface ICatalogService
+    {
+        Task<List<CourseViewModel>> GetAllCourseAsync();
+        Task<List<CourseViewModel>> GetAllCourseGetByUserIdAsync(string userId);
+        Task<List<CourseViewModel>>GetByCourseId(string courseId);
+        Task<bool>DeleteCourseAsync(string courseId);
+        Task<bool>CreateCourseAsync(CourseCreateInput courseCreateInput);
+        Task<bool> UpdateCourseAsync(CourseUpdateInput courseUpdateInput);
+
+
+    }
+}
